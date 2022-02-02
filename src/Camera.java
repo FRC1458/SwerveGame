633e50e88a1c90086a129dfb8d14d10d67511870
ThirtyNinene <thirtyninene@gmail.com>
2022-02-01 16:37:46 -0800

    simplified camera.java

--- src/Camera.java
+++ src/Camera.java
@@ -1,15 +1,9 @@
 public class Camera {
-    private float ballX;
-    private float ballY;
-    public Camera(float ballX, float ballY) {
-        this.ballX = ballX;
-        this.ballY = ballY;
-        SwerveGui Coords = new SwerveGui();
+
+    public float distanceX(float ballX, float robotX) {
+        return (ballX - robotX);
     }
-    public void distanceX(float ballX, float Coords.getRobotX()) {
-        return (ballX - Coords.getRobotX());
-    }
-    public void distanceY(float ballY, float Coords.getRobotY()) {
-        return (ballY - Coords.getRobotY());
+    public float distanceY(float ballY, float robotY) {
+        return (ballY - robotY);
     }
 }
