926b90dd145545b90cea796197df7773c790c83d
LiamKDolan <liamkdolan@gmail.com>
2022-02-05 10:08:09 -0800

    Update Camera.java

--- src/Camera.java
+++ src/Camera.java
@@ -2,19 +2,20 @@ import java.lang.Math;
 public class Camera {
     SwerveGui Coords = new SwerveGui();
     public double distanceX(double ballX, double robotX) {
-        return (ballX - robotX);
+        return Math.abs((ballX - robotX));
     }
     public double distanceY(double ballY, double robotY) {
-        return (ballY - robotY);
+        return Math.abs((ballY - robotY));
     }
-    double ballToRobotX = distanceX(Coords.randomInt1, Coords.getRobotX());
-    double ballToRobotY = distanceY(Coords.randomInt2, Coords.getRobotY());
+    double vectorX = distanceX(Coords.randomInt1, Coords.getRobotX());
+    double vectorY1 = distanceY(Coords.randomInt2, Coords.getRobotY());
     public double angle() {
-        double angle = Math.atan(ballToRobotY/ballToRobotX);
-        return angle;
+        double angle = Math.acos((vectorX*hypotenuse())/(Math.abs(vectorX))*Math.abs(hypotenuse()));
+        System.out.println(angle*180/Math.PI);
+        return angle*180/Math.PI;
     }
     public double hypotenuse() {
-        double hypotenuse = Math.sqrt(Math.pow(ballToRobotX,2)+Math.pow(ballToRobotY,2));
+        double hypotenuse = Math.sqrt(Math.pow(vectorX,2)+Math.pow(vectorY1,2));
         return hypotenuse;
     }
 }
